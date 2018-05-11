5c1bf5681979f396f6ce330ba521a6c89463436a
wang <essentialva@163.com>
2018-05-11 09:56:43 +0800

    5.11

--- src/main/java/com/FM/controller/player/PlayController.java
+++ src/main/java/com/FM/controller/player/PlayController.java
@@ -107,47 +107,63 @@ public class PlayController {
     @ResponseBody
     public Map<String,Object> getOtherSong(@RequestParam("id") Integer id,@RequestParam("music_id") Integer music_id,@RequestParam("signal") Integer signal){
         List<Course> list = courseService.getCourse(id);
+        Type ll2Type = typeService.getType(id);
+        Type ll1Type = typeService.getType(ll2Type.getParentType());
+
         Boolean result = false;
         String desc="";
         Course rtCourse=null;
-        Map<String,Type> types=null;
+        Integer i = 0;
+        Map<String,Type> types= new HashMap<>();
+        types.put("l1",ll1Type);
+        types.put("l2",ll2Type);
+
         Map<String,String> rtArr=new HashMap<>();
-        for(int i=0;i<list.size();i++){
-            Course course = list.get(i);
-            if(course.getId()==music_id) {
-                if (i + signal < list.size() && i + signal > -1) {
+        for(int inum=0;inum<list.size();inum++) {
+            Course lcourse = list.get(inum);
+            if(lcourse.getId()==music_id) {
+                i=inum;
+                break;
+            }
+        }
+                if(i + signal < list.size() && i + signal > -1) {
                     rtCourse = list.get(i + signal);
-                    System.out.println("rt:"+rtCourse.getCourseName());
                     Type l2Type = typeService.getType(rtCourse.getType());
                     Type l1Type = typeService.getType(l2Type.getParentType());
                     rtArr.put("sorting_id", l1Type.getId().toString());
                     rtArr.put("sorting_name", l1Type.getTypeName());
                     rtArr.put("subtype_id", l2Type.getId().toString());
                     rtArr.put("subtype_name", l2Type.getTypeName());
-                    break;
-                } else if (i + signal <= -1) {
-                    types = typeService.getNextType(id, signal);
-                    List<Course> llist = courseService.getCourse(types.get("l2").getId());
+                }
+                if (i + signal <= -1) {
+
+                    List<Course> llist=null;
+                    while(llist==null || llist.size()==0) {
+                        types = typeService.getNextType(types.get("l2").getId(), signal);
+                        llist = courseService.getCourse(types.get("l2").getId());
+                    }
                     if(llist!=null)
                     rtCourse = llist.get(llist.size() - 1);
                     rtArr.put("sorting_id", types.get("l1").getId().toString());
                     rtArr.put("sorting_name", types.get("l1").getTypeName());
                     rtArr.put("subtype_id", types.get("l2").getId().toString());
                     rtArr.put("subtype_name", types.get("l2").getTypeName());
-                    break;
                 } else if (i + signal >= list.size()) {
-                    types = typeService.getNextType(id, signal);
-                    List<Course> llist = courseService.getCourse(types.get("l2").getId());
+                    List<Course> llist=null;
+                    while(llist==null || llist.size()==0) {
+                        types = typeService.getNextType(types.get("l2").getId(), signal);
+                        System.out.println(types.get("l2").getTypeName());
+                        llist = courseService.getCourse(types.get("l2").getId());
+                        System.out.println(llist.size());
+                    }
                     if(llist!=null)
                     rtCourse = llist.get(0);
                     rtArr.put("sorting_id", types.get("l1").getId().toString());
                     rtArr.put("sorting_name", types.get("l1").getTypeName());
                     rtArr.put("subtype_id", types.get("l2").getId().toString());
                     rtArr.put("subtype_name", types.get("l2").getTypeName());
-                    break;
                 }
-            }
-        }
+
         if(rtCourse!=null)
         {
             result=true;
